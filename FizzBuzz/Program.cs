fc9b2d24e238b94f60087c40aea1a68c0c03bcb1
Tasmia Zahin <tasmiamaithily@gmail.com>
2022-12-28 23:55:53 +0100

    First commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FizzBuzz;
+class Program
+{
+    static void Main(string[] args)
+    
+
+{
+        int number;
+        Console.WriteLine("enter a number");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        if (number % 3 == 0 & number % 5 == 0)
+        {
+            Console.WriteLine("FizzBuzz");
+        }
+        else if (number % 3 == 0)
+        {
+            Console.WriteLine("Fizz");
+        }
+        else if (number % 5 == 0)
+        {
+            Console.WriteLine("Buzz");
+        }
+        else
+        {
+            Console.WriteLine("Your number is not FizzBuzz");
+        }
+
+        Console.ReadLine();
+
+    }
+}
+
+
+
